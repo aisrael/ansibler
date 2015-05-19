244e71d6b13d904e03c8b4d74a5991232c6d533c
Alistair A. Israel <aisrael@gmail.com>
2015-05-19 21:09:45 +0800

    Added a scenario, and fixed a bug while we're at it

--- lib/ansible/inventory.rb
+++ lib/ansible/inventory.rb
@@ -4,11 +4,10 @@ module Ansible
 
     class << self
       def read_file(file)
-        puts "Ansibler::Inventory.read_file(\"#{file}\")"
         inventory = Inventory.new
         File.foreach(file) do |line|
           case
-          when line =~ /^\s*\S+\s+(\S+=\S+\s*)*$/
+          when line =~ /^\s*\S+\s*(\S+=\S+\s*)*$/
             host_name, *rest = line.split
             vars = Hash[rest.map {|s| s.split('=')}]
             host = Host.new host_name, vars
